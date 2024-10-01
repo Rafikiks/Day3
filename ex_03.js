da8926fda9b1b0073d41b23c79ac8ddf0e4cf5e2
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-01 10:28:01 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,16 @@
+function changebackground() {
+  
+
+  if (magicNumber === 1) {
+    paintRedRectangle();
+    var magicNumber = 1;
+  }
+  if (magicNumber === 2) {
+    paintGreenRectangle();
+    let magicNumer = 2
+  }
+  if (magicNumber === 3) {
+    paintYellowRectangle();
+    let magicNumber = 3
+  }
+}

ca4c24c8ef972301fa80c84b5269e5a9cc2ed361
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-01 18:15:50 +0200

    Update ex_05bis.js

--- ex_05bis.js
+++ ex_05bis.js
@@ -1,22 +1,24 @@
 function getBasketContent(nbFruits) {
-let strawberry = "strawberry"
-let apple = "apple"
-let lime = "lime"
-let peach = "peach"
-let pear = "pear"
+    let strawberry = 'strawberry';
+    let apple = 'apple';
+    let lime = 'lime';
+    let peach = 'peach';
+    let pear = 'pear';
+    
+    let fruits = [strawberry, strawberry, strawberry, apple, apple, lime, lime, peach, pear, pear];
 
-let fruits = [strawberry,strawberry,strawberry,apple,apple,lime,lime,peach,pear,pear]
-	
- if (nbFruits > fruits.length) {
- console.log("too many fruit(s) selected")
- return []
- }
-  else {
-  	console.log(nbFruits + ' fruit(s) selected')
-  	return fruits.slice(0, nbFruits)
- }
+    if (nbFruits > fruits.length) {
+        console.log("Too many fruit(s) selected.");
+        return [];
+    } else {
+        console.log(nbFruits + ' fruit(s) selected');
+        return fruits.slice(0, nbFruits);
+    }
 }
+
 if (typeof getBasketContent === "function") {
-	const fruits = getBasketContent(11)
-	showMyBasket(fruits)
+    const fruits = getBasketContent(10);
+    showMyBasket(fruits);
 }
+
+// La fonction retourne les fruits demandés ou un message d'erreur si le nombre est trop élevé.

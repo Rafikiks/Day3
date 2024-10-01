b69c3fbfcf8178938ff9f6488586cb3ab9bd590c
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-10-01 17:57:28 +0200

    Update ex_09bis.js

--- ex_09bis.js
+++ ex_09bis.js
@@ -1,34 +1,37 @@
-
 function splitOperation(operation) {
-    var parts = operation.split(' ');
-    var num1 = parseInt(parts[0]);
-    var operator = parts[1];
-    var num2 = parseInt(parts[2]);
-    return [num1, operator, num2];
+    let parts = operation.split(' ');
+    let firstNumber = parseInt(parts[0]);
+    let operator = parts[1];
+    let secondNumber = parseInt(parts[2]);
+    return [firstNumber, operator, secondNumber];
 }
-
 function doOperation(operation) {
-    var parts = splitOperation(operation);
-    var num1 = parts[0];
-    var operator = parts[1];
-    var num2 = parts[2];
-    
-    if (operator == '+') {
-        return num1 + num2;
-    }
-    if (operator == '-') {
-        return num1 - num2;
-    }
-    if (operator == '*') {
-        return num1 * num2;
-    }
-    if (operator == '/') {
-        if (num2 == 0) {
-            console.log("Can't divide by 0");
+    let [firstNumber, operator, secondNumber] = splitOperation(operation);
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            if (secondNumber === 0) {
+                console.log("Can't divide by 0");
+                return null;
+            } else {
+                result = firstNumber / secondNumber;
+            }
+            break;
+        default:
+            console.log("Opérateur non reconnu");
             return null;
-        }
-        return num1 / num2;
     }
-    return null;
+    return result;
 }
 
+// La fonction doOperation effectue l'opération mathématique basée sur l'opérateur donné
